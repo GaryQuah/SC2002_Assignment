0cdf07c339ca044243bc4e33240b9c27959764e6
Andrea <andreawong9041@gmail.com>
2024-10-23 16:04:35 +0800

    added patient and updated appointment

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -1,38 +1,122 @@
 package models;
+
+import java.util.List;
+
+import ServiceClasses.Appointment;
+import ServiceClasses.AppointmentManager;
 import models.enums.Gender;
 import models.enums.Role;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
-public class Patient extends User{
-    //------------------ Variables -----------------------------
+public class Patient extends User {
+    // ------------------ Variables -----------------------------
     private String m_PatientID;
     private String m_Name;
     private String m_DateOfBirth;
     private Gender m_Gender;
     private String m_EmailAddress;
     private int m_PhoneNumber;
-    private String m_BloodType;
+    // private String m_BloodType;
+    // private List<MedicalRecord> m_MedicalRecords; // update this
 
-    //------------------- Functions ---------------------------
+    // ------------------- Functions ---------------------------
 
-    //Creates a "Patient" after passing the user's username. default password is "password", default role is "Patient"
+    AppointmentManager appointmentManager = new AppointmentManager();
+
+    // Creates a "Patient" after passing the user's username. default password is
+    // "password", default role is "Patient"
     public Patient(String m_UserName) {
         super(m_UserName, Role.Patient); // Calls the constructor of the User class
-    }
-   
-    //To Add Past Diagnoses and treatments when its clearer in the document what it will e
-    //m_Diagnoses
-    //m_Treatments
 
-    //Patients can update non-medical personal information such as email address and contact number. 
-    public void UpdateEmail(String m_EmailString)
-    {
-        this.m_EmailAddress= m_EmailString;
     }
 
-    public void UpdatePhoneNumber(int m_PhoneNumber)
-    {
+    // Patients can update non-medical personal information such as email address
+    // and contact number.
+    public void updateEmail(String m_EmailString) {
+        this.m_EmailAddress = m_EmailString;
+    }
+
+    public void updatePhoneNumber(int m_PhoneNumber) {
         this.m_PhoneNumber = m_PhoneNumber;
     }
 
-    //Patients are not allowed to modify the past diagnoses, prescribed medications, treatments or blood type.. 
+    // check appointment manager methods...
+    // Schedule appointment
+    // choose doctor, date, available time slot
+    public void scheduleAppointment(String doctorName, String appointmentDate,
+            String timeSlot, String appointmentType, int appointmentID) {
+
+        boolean isSlotAvailable = appointmentManager.ScheduleAppointment(doctorName, m_Name, appointmentDate, timeSlot,
+                appointmentType);
+        if (isSlotAvailable) {
+            System.out.println("Appointment scheduled successfully.");
+        } else {
+            System.out.println("Failed to schedule appointment.");
+        }
+
+    }
+
+    // cancel patient appointment
+    public void cancelPatientAppointment(String doctorName, String appointmentDate,
+            String timeSlot) {
+        boolean isCancelled = appointmentManager.CancelAppointment(doctorName, m_Name, appointmentDate, timeSlot);
+        if (isCancelled) {
+            System.out.println("Appointment cancelled.");
+        } else {
+            System.out.println("Failed to cancel appointment.");
+        }
+    }
+
+    // reschedule patient appointment
+    public void reschedulePatientAppointment(String doctorName, String newAppointmentDate,
+            String newTimeSlot, String appointmentType, String oldAppointmentDate, String oldTimeSlot) {
+        appointmentManager.ReScheduleAppointment(doctorName, m_Name, newAppointmentDate, newTimeSlot, appointmentType,
+                oldAppointmentDate, oldTimeSlot);
+    }
+
+    public void getPastPatientAppointments() {
+        Vector<Appointment> pastAppointments = appointmentManager.getPastAppointments(m_Name);
+
+        if (pastAppointments.isEmpty()) {
+            System.out.println("No past apppointments found.");
+        } else {
+            for (Appointment appointment : pastAppointments) {
+                System.out.println(appointment);
+            }
+        }
+    }
+
+    // view scheduled appointments
+    public void viewPatientScheduledAppointments() {
+        appointmentManager.ViewPatientAppointments(m_Name);
+    }
+
+    public void viewAvailableAppointments(String doctorName) {
+        appointmentManager.ViewAvailableDates(doctorName);
+    }
+
+    /*
+     * // To view medical records
+     * // shift this to view
+     * public void ViewMedicalRecord() {
+     * System.out.println("Name: " + m_Name);
+     * System.out.println("Patient ID: " + m_PatientID);
+     * System.out.println("Date of Birth: " + m_DateOfBirth);
+     * System.out.println("Gender: " + m_Gender);
+     * System.out.println("Contact Number: " + m_PhoneNumber);
+     * System.out.println("Email: " + m_EmailAddress);
+     * System.out.println("Blood Type: " + m_BloodType);
+     * System.out.println("------ Medical History ------");
+     * 
+     * for (MedicalRecord record : m_MedicalRecords) {
+     * // print records
+     * }
+     * 
+     * }
+     */
+
+    // Patients are not allowed to modify the past diagnoses, prescribed
+    // medications, treatments or blood type..
 }
--- src/view/PatientMenu.java
+++ src/view/PatientMenu.java
@@ -1,5 +1,126 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import models.Patient;
+
 public class PatientMenu {
-    
+    // private static List<Patient> patientList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int choice;
+        Scanner sc = new Scanner(System.in);
+
+        // example for testing
+        Patient patient = new Patient("john_doe");
+
+        do {
+            System.out.println("--------------------------------");
+            System.out.println("Patient Menu");
+            System.out.println("--------------------------------");
+            System.out.println("1. View Medical Record");
+            System.out.println("2. Update Personal Information");
+            System.out.println("3. View Available Appointment Slots");
+            System.out.println("4. Schedule an Appointment");
+            System.out.println("5. Reschedule an Apppointment");
+            System.out.println("6. Cancel an Appointment");
+            System.out.println("7. View Scheduled Appointments");
+            System.out.println("8. View Past Appointment Outcome Records");
+            System.out.println("9. Logout");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // consume newline
+
+            switch (choice) {
+                case 1:
+                    break;
+
+                case 2: {
+                    System.out.println("Please enter new email: ");
+                    String newEmail = sc.nextLine();
+                    patient.updateEmail(newEmail);
+                    System.out.print("Enter new phone number: ");
+                    int newPhone = sc.nextInt();
+                    sc.nextLine(); // Consume newline
+                    patient.updatePhoneNumber(newPhone);
+                    System.out.println("Personal information updated successfully.");
+
+                    break;
+                }
+
+                case 3: {
+                    System.out.print("Enter doctor name: ");
+                    String doctorName = sc.nextLine();
+                    patient.viewAvailableAppointments(doctorName);
+                    break;
+                }
+
+                case 4: {
+                    System.out.print("Enter patient's name: ");
+                    String patientName = sc.nextLine();
+                    System.out.print("Enter doctor name: ");
+                    String doctorName = sc.nextLine();
+                    System.out.print("Enter appointment date (yyyy-mm-dd): ");
+                    String date = sc.nextLine();
+                    System.out.print("Enter time slot (HH:mm): ");
+                    String timeSlot = sc.nextLine();
+                    System.out.print("Enter appointment type: ");
+                    int type = sc.nextInt();
+                    sc.nextLine(); // consume newline
+
+                    patient.scheduleAppointment(doctorName, patientName, date, timeSlot, type);
+                    break;
+                }
+
+                case 5: {
+                    System.out.print("Enter doctor name: ");
+                    String rescheduleDoctorName = sc.nextLine();
+                    System.out.print("Enter current appointment date (yyyy-mm-dd): ");
+                    String oldDate = sc.nextLine();
+                    System.out.print("Enter current time slot (HH:mm): ");
+                    String oldTimeSlot = sc.nextLine();
+                    System.out.print("Enter new appointment date (yyyy-mm-dd): ");
+                    String newDate = sc.nextLine();
+                    System.out.print("Enter new time slot (HH:mm): ");
+                    String newTimeSlot = sc.nextLine();
+                    System.out.print("Enter appointment type: ");
+                    String newType = sc.nextLine();
+
+                    patient.reschedulePatientAppointment(rescheduleDoctorName, newDate, newTimeSlot, newType, oldDate,
+                            oldTimeSlot);
+
+                    break;
+                }
+
+                case 6: {
+                    System.out.print("Enter doctor name: ");
+                    String cancelDoctorName = sc.nextLine();
+                    System.out.print("Enter appointment date (yyyy-mm-dd): ");
+                    String cancelDate = sc.nextLine();
+                    System.out.print("Enter time slot (HH:mm): ");
+                    String cancelTimeSlot = sc.nextLine();
+                    patient.cancelPatientAppointment(cancelDoctorName, cancelDate, cancelTimeSlot);
+
+                    break;
+                }
+
+                case 7:
+                    patient.viewPatientScheduledAppointments();
+                    break;
+
+                case 8:
+                    patient.getPastPatientAppointments();
+                    break;
+
+                case 9:
+                    System.out.println("Logging out...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 9);
+
+    }
 }
