3cf938b0c2b7a26f3470772fe1c1397b6e110449
Andrea <andreawong9041@gmail.com>
2024-10-28 16:57:25 +0800

    updated medical record

--- src/models/MedicalRecord.java
+++ src/models/MedicalRecord.java
@@ -1,34 +1,123 @@
 package models;
 
+import models.enums.Gender;
+
 public class MedicalRecord {
     private String patientId;
     private String doctorId;
     private String diagnosis;
     private String treatment;
     private String date;
+    private String name;
+    private Gender gender;
+    private String emailAddress;
+    private String phoneNumber;
+    private String bloodType;
+    private String dateOfBirth;
 
     // Constructor
-    public MedicalRecord(String patientId, String doctorId, String diagnosis, String treatment, String date) {
+    public MedicalRecord(String patientId, String doctorId, String diagnosis, String treatment, String date,
+            String name, Gender gender, String emailAddress,
+            String phoneNumber, String bloodType, String dateOfBirth) {
         this.patientId = patientId;
         this.doctorId = doctorId;
         this.diagnosis = diagnosis;
         this.treatment = treatment;
         this.date = date;
+        this.name = name;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.bloodType = bloodType;
+        this.dateOfBirth = dateOfBirth;
     }
 
     // Getters and Setters
-    public String getPatientId() { return patientId; }
-    public void setPatientId(String patientId) { this.patientId = patientId; }
+    public String getName() {
+        return name;
+    }
 
-    public String getDoctorId() { return doctorId; }
-    public void setDoctorId(String doctorId) { this.doctorId = doctorId; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getDiagnosis() { return diagnosis; }
-    public void setDiagnosis(String diagnosis) { this.diagnosis = diagnosis; }
+    public Gender getGender() {
+        return gender;
+    }
 
-    public String getTreatment() { return treatment; }
-    public void setTreatment(String treatment) { this.treatment = treatment; }
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
